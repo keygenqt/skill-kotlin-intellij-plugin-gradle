071eaee8225dcc66797ebd045dc19e225d8176b1
Vitaliy Zarubin <keygenqt@pinxterapp.com>
2020-05-05 00:01:20 +0300

    Add Apache 2.0

--- src/main/kotlin/com/keygenqt/plugin/simple/util/SimpleUtil.kt
+++ src/main/kotlin/com/keygenqt/plugin/simple/util/SimpleUtil.kt
@@ -1,15 +1,28 @@
+/*
+ * Copyright 2020 Vitaliy Zarubin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.keygenqt.plugin.simple.util
 
-import com.intellij.navigation.*
 import com.intellij.openapi.project.*
 import com.intellij.psi.*
 import com.intellij.psi.search.*
 import com.intellij.psi.util.*
 import com.keygenqt.plugin.simple.file.*
-import com.keygenqt.plugin.simple.language.*
 import org.intellij.sdk.language.psi.*
 import java.util.*
-import javax.swing.*
 
 class SimpleUtil {
 
